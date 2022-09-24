ed8dc6ea01443f573bbe53012b1b4ee8e1367c04
beharrell <mark.beharrell@airways.co.nz>
2022-09-24 19:08:11 +1200

    split kernels to header and body

---
+++ src/Kernels.cpp
@@ -0,0 +1,69 @@
+#include "Kernels.h"
+
+bool BinaryErrode::Process(uint8_t pixel)
+{
+    mResult = pixel == 0 ? 0 : 255;
+    return mResult == 0;
+}
+
+bool LayeredDilate::Process(uint8_t pixel)
+{
+    mResult = pixel != 0 ? 0x80 : 0;
+    return mResult != 0;
+}
+
+void LayeredDilate::UpdatePixel(uint8_t &pixel)
+{
+    pixel = pixel >> 1 | Result();
+}
+
+bool Gaussian::Process(uint8_t pixel)
+{
+    assert(mGausIdx < 9);
+    mResult += mGaussian[mGausIdx] * pixel;
+    return ++mGausIdx == 9;
+}
+
+uint8_t Gaussian::Result() { return mResult / 16; }
+
+void Gaussian::Reset()
+{
+    Kernel::Reset();
+    mGausIdx = 0;
+}
+
+void Convolve::Process(const uint16_t i, const uint16_t j)
+{
+    mKernel->Reset();
+
+    for (int y = j - mHalfKernelSize; y <= j + mHalfKernelSize; ++y)
+    {
+        auto row = mSourceImage + (y * mMaxI);
+        for (int x = i - mHalfKernelSize; x <= i + mHalfKernelSize; ++x)
+        {
+            auto stop = mKernel->Process(row[x]);
+            if (stop)
+            {
+                break;
+            }
+        }
+    }
+}
+
+void Convolve::Process(Kernel *kernel, uint8_t *sourceImage, uint8_t *destImage)
+{
+    mKernel = kernel;
+    mSourceImage = sourceImage;
+    mDestImage = destImage;
+    mHalfKernelSize = mKernel->HalfKernelSize();
+
+    for (int j = mHalfKernelSize; j < mMaxJ - mHalfKernelSize; ++j)
+    {
+        auto row = mDestImage + (j * mMaxI);
+        for (int i = mHalfKernelSize; i < mMaxI - mHalfKernelSize; ++i)
+        {
+            Process(i, j);
+            mKernel->UpdatePixel(row[i]);
+        }
+    }
+}
--- src/Kernels.h
+++ src/Kernels.h
@@ -7,55 +7,35 @@ class Kernel
 private:
    int16_t mHalfKernelSize{1};
 
+protected:
+  uint8_t mResult{0};
+
 public:
   Kernel(int16_t halfKernelSize = 1):mHalfKernelSize{halfKernelSize}{}
+  virtual ~Kernel(){};
+
   virtual bool Process(uint8_t pixel) = 0;
-  virtual uint8_t Result() = 0;
-  virtual void Reset() = 0;
+  virtual uint8_t Result() { return mResult; }
+  virtual void Reset(){mResult = 0;}
   virtual void UpdatePixel(uint8_t &pixel) {pixel = Result();}
   virtual int16_t HalfKernelSize(){return mHalfKernelSize;};
 };
 
 class BinaryErrode : public Kernel
 {
-private:
-  uint8_t mResult{0};
-
 public:
   using Kernel::Kernel;
 
-  bool Process(uint8_t pixel) override
-  {
-    mResult = pixel == 0 ? 0 : 255;
-    return mResult == 0;
-  }
-
-  uint8_t Result() override { return mResult; }
-
-  void Reset() override { mResult = 0; }
+  bool Process(uint8_t pixel) override;
 };
 
 class LayeredDilate : public Kernel
 {
-private:
-  uint8_t mResult{0};
-
 public:
   using Kernel::Kernel;
 
-  bool Process(uint8_t pixel) override
-  {
-    mResult = pixel != 0 ? 0x80 : 0;
-    return mResult != 0;
-  }
-
-  uint8_t Result() override { return mResult; }
-
-  void UpdatePixel(uint8_t &pixel) {
-    pixel = pixel >> 1 | Result();
-  }
-
-  void Reset() override { mResult = 0; }
+  bool Process(uint8_t pixel) override;
+  void UpdatePixel(uint8_t &pixel);
 };
 
 class Gaussian : public Kernel
@@ -70,20 +50,9 @@ private:
 public:
   Gaussian():Kernel(1){}
 
-  bool Process(uint8_t pixel) override
-  {
-    assert(mGausIdx < 9);
-    mResult += mGaussian[mGausIdx] * pixel;
-    return ++mGausIdx == 9;
-  }
-
-  uint8_t Result() override { return mResult / 16; }
-
-  void Reset() override
-  {
-    mResult = 0;
-    mGausIdx = 0;
-  }
+  bool Process(uint8_t pixel) override;
+  uint8_t Result() override;
+  void Reset() override;
 };
 
 class Convolve
@@ -94,47 +63,14 @@ private:
   Kernel *mKernel;
   uint8_t *mSourceImage;
   uint8_t *mDestImage;
-  int16_t halfKernelSize;
+  int16_t mHalfKernelSize;
 
-  void Process(const uint16_t i, const uint16_t j)
-  {
-    mKernel->Reset();
-
-    for (int y = j - halfKernelSize; y <= j + halfKernelSize; ++y)
-    {
-      auto row = mSourceImage + (y * mMaxI);
-      for (int x = i - halfKernelSize; x <= i + halfKernelSize; ++x)
-      {
-        auto stop = mKernel->Process(row[x]);
-        if (stop)
-        {
-          break;
-        }
-      }
-    }
-  }
+  void Process(const uint16_t i, const uint16_t j);
 
   public:
   Convolve(int maxI, int maxJ):mMaxI{maxI}, mMaxJ{maxJ}{}
 
-  void Process(Kernel *kernel, uint8_t *sourceImage, uint8_t *destImage)
-  {
-    mKernel = kernel;
-    mSourceImage = sourceImage;
-    mDestImage = destImage;
-    halfKernelSize = mKernel->HalfKernelSize();
-
-    for (int j = halfKernelSize; j < mMaxJ - halfKernelSize; ++j)
-    {
-      auto row = mDestImage + (j * mMaxI);
-      for (int i = halfKernelSize; i < mMaxI - halfKernelSize; ++i)
-      {
-        Process(i, j);
-        mKernel->UpdatePixel(row[i]);
-      }
-    }
-
-  }
+  void Process(Kernel *kernel, uint8_t *sourceImage, uint8_t *destImage);
 };
 
 
